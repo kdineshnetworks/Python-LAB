c91b1c21361df095d9e8604ac391d2da17a7d067
dinesh <kdineshnetworks@gmail.com>
2026-03-18 17:30:39 -0400

    Initial commit: Add Circle, Rectangle, and kmtomiles Python files

---
+++ Pratice/Circle.py
@@ -0,0 +1,4 @@
+import math
+r=int(input("Enter the radius of the circle: "))
+area=math.pi*r**2
+print("area",area)
---
+++ Pratice/kmtomiles.py
@@ -0,0 +1,3 @@
+km=float(input("Enter the distance in kilometers: "))
+miles=0.621371*km
+print("miles",miles)
