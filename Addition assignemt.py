90f40ac362f66c2a87209c9c025ce524f32b86fb
dinesh <kdineshnetworks@gmail.com>
2026-03-18 18:49:39 -0400

    commit

---
+++ Addition assignemt.py
@@ -0,0 +1,7 @@
+a=5
+a=a+1
+print(a)
+
+b=5
+b+=1
+print(b)
---
+++ Quadratic Equation.py
@@ -0,0 +1,7 @@
+import math
+a=1
+b=-5
+c=6
+r1=(-b+math.sqrt(b**2-4*a*c))/(2*a)
+r2=(-b-math.sqrt(b**2-4*a*c))/(2*a)
+print("The roots of the quadratic equation are:",r1,"and",r2)
